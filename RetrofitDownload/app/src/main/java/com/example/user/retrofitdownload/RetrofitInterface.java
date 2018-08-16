2c91636e2baf7393096bb4e3853465ae611b51ae
panaghiageorge <38526106+panaghiageorge@users.noreply.github.com>
2018-08-16 11:22:08 +0300

    RetrofitDowload
    Task 13

---
+++ RetrofitDownload/app/src/main/java/com/example/user/retrofitdownload/RetrofitInterface.java
@@ -0,0 +1,12 @@
+package com.example.user.retrofitdownload;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Streaming;
+import retrofit2.http.Url;
+public interface RetrofitInterface {
+    @Streaming
+    @GET
+    Call<ResponseBody> downloadFileByUrl(@Url String fileUrl);
+}
